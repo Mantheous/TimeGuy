e37dc6897dd189bab96ba8a64152fbb91ae0559a
Mantheous <Mantheous@gmail.com>
2020-04-11 09:37:54 -0600

    Auto Aim Drones & level3

--- Time Guy/Assets/Scripts/DroneMove.cs
+++ Time Guy/Assets/Scripts/DroneMove.cs
@@ -16,7 +16,7 @@ public class DroneMove : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        target = GameObject.FindGameObjectWithTag("Player").transform;
     }
 
     // Update is called once per frame
