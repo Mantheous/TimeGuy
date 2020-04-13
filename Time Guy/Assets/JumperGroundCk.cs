044843610993d4411826cd36d54cf3a38c95cd0b
Mantheous <Mantheous@gmail.com>
2020-04-13 16:29:02 -0600

    Level3stuff

---
+++ Time Guy/Assets/JumperGroundCk.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumperGroundCk : MonoBehaviour
+{
+    public JumperMove jumperMove;
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "ground")
+            jumperMove.groundSet(true);
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "ground")
+            jumperMove.groundSet(false);
+    }
+}
---
+++ Time Guy/Assets/JumperMove.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumperMove : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    Transform playerPos;
+    public float jumpheight;
+    public float jumpDis;
+    public bool grounded;
+    float jumpDisF;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerPos = GameObject.FindGameObjectWithTag("Player").transform; 
+    }
+
+    private void FixedUpdate()
+    {
+        if(transform.position.x < playerPos.position.x)
+        {
+            transform.localScale = new Vector3(-5, 5, 1);
+            jumpDisF = -jumpDis;
+        }else
+        {
+            transform.localScale = new Vector3(5, 5, 1);
+            jumpDisF = jumpDis;
+        }
+
+        if(grounded)
+        {
+            //Jump up
+            rb.AddForce(new Vector2(jumpDisF, jumpheight));
+
+            
+            grounded = false;
+        }
+    }
+
+    public void groundSet(bool ground_)
+    {
+        grounded = ground_;
+        //Debug.Log("groundSet");
+    }
+}
