6ed21fd7ce9cbfc216b5c00bab3747f61ee83eb0
Mantheous <Mantheous@gmail.com>
2020-04-18 17:57:12 -0600

    Level6

--- Time Guy/Assets/Scripts/TimeBody.cs
+++ Time Guy/Assets/Scripts/TimeBody.cs
@@ -22,7 +22,7 @@ public class TimeBody : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetButtonDown("Fire3"))
+        if(Input.GetButton("Fire3"))
         {
             StartRewind();
             //Debug.Log("Start Rewind");
